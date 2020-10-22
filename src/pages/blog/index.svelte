d33e2154877d8641bf0ac82f4c91ba5c739fe396
heliosnarcissus <reyanthonyrenacia@gmail.com>
2020-10-22 11:18:16 +0800

    blog starter template

---
+++ src/pages/blog/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { url, layout } from "@sveltech/routify";
+
+  const posts = $layout.parent.children
+   .filter((c) => c.meta["frontmatter"]);
+    //.sort((a, b) => b.meta["frontmatter"].published.localeCompare(a.meta["frontmatter"].published));
+
+
+    console.log(posts);
+</script>
+
+<h1 class="bg-teal-500">Helios</h1>
+
+<ul class="posts">
+  {#each posts as {meta, path}}
+    <li class="card">
+      <a class="title" href={$url(path)}>{meta.frontmatter.title}</a>
+      {@html (meta.frontmatter.summary)}
+    </li>
+  {/each}
+</ul>
